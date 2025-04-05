db20501bb35dd231434f8a475af0efcfa77e12ed
oncatt1 <oncattgamerpl@gmail.com>
2025-04-05 19:52:11 +0200

    quickfix

--- main.py
+++ main.py
@@ -21,6 +21,34 @@ def run_nmap_with_password(cmd, password):
     
     return process.returncode
 
+def scan_active_hosts(subnet, range_start, range_end):
+    cmd = f"sudo nmap -sn {subnet}.{range_start}-{range_end} -oX logs/hosts.xml"
+    subprocess.run(cmd, shell=True, text=True)
+    
+    with open("logs/hosts.xml", "r") as file:
+        data = xmltodict.parse(file.read())
+    
+    hosts = []
+    if 'host' in data['nmaprun']:
+        host_data = data['nmaprun']['host']
+        if isinstance(host_data, list):
+            for host in host_data:
+                if isinstance(host['address'], list):
+                    for addr in host['address']:
+                        if addr['@addrtype'] == 'ipv4':
+                            hosts.append(addr['@addr'])
+                elif host['address']['@addrtype'] == 'ipv4':
+                    hosts.append(host['address']['@addr'])
+        elif isinstance(host_data, dict):
+            if isinstance(host_data['address'], list):
+                for addr in host_data['address']:
+                    if addr['@addrtype'] == 'ipv4':
+                        hosts.append(addr['@addr'])
+            elif host_data['address']['@addrtype'] == 'ipv4':
+                hosts.append(host_data['address']['@addr'])
+    
+    return hosts
+
 def scan_open_ports(subnet, range_start, range_end):
     hosts = scan_active_hosts(subnet, range_start, range_end)
     for ip in hosts:
@@ -71,7 +99,7 @@ def scan_hosts():
         
         with open("logs/hosts.xml", "r") as file:
             data = xmltodict.parse(file.read())
-        
+            
         hosts = []
         if 'host' in data['nmaprun']:
             host_data = data['nmaprun']['host']
